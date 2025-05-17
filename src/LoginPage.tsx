8d611c9157fafea13a23129764ce154cbdd77bc8
양재희 <happyjessie0318@gmail.com>
2025-05-18 04:03:22 +0900

    로그인 페이지 구현 완료

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,31 +1,77 @@
 import { useNavigate } from "react-router-dom";
 import "./LoginPage.css";
+import EmailIcon from "./assets/email.svg"; 
+import PasswordIcon from "./assets/password.svg"; 
+import ArrowIcon from "./assets/arrow.svg"; 
+import ArrowIconClick from "./assets/arrow_click.svg"; 
+import EyeOpen from "./assets/eye_open.svg";
+import EyeClosed from "./assets/eye_close.svg";
+import { useState } from "react";
+
 
 const LoginPage = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-    const toWaitingPage = () => {
-        navigate("/waiting");
-    }
-
-    const toStudentPage = () => {
-        navigate("/student");
-    }
-
-    const toProfPage = () => {
+    const handleLogin = () => {
+        if (email === "admin@example.com" && password === "admin123") {
         navigate("/prof");
-    }
+        } else if (email === "student@example.com" && password === "student123") {
+        navigate("/student");
+        } else {
+        setError(true); 
+        }
+    };
 
+    const toggleShowPassword = () => setShowPassword(prev => !prev);
 
     return (
         <div className="login-page-container">
             <div className="login-content">
-                <button onClick={toWaitingPage}>대기 페이지 이동 버튼</button>
-                <button onClick={toStudentPage}>학생 페이지 이동 버튼</button>
-                <button onClick={toProfPage}>강사 페이지 이동 버튼</button>
-                <div>여긴 로그인 페이지-나중에 구현해야 함</div>
-            </div>
-        </div>
+            <h1 className="title">WELCOME</h1>
+            
+                <div className = "email-input-wrapper">
+                    <img src={EmailIcon} alt="email" className = "email-icon" />
+                    <input 
+                        className="email-input" 
+                        type="email" 
+                        placeholder="Email" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                                
+                <div className="password-input-wrapper">
+                    <img src={PasswordIcon} alt="password" className="password-icon" />
+                        <input
+                            type={showPassword ? "password" : "text"}
+                            placeholder="Password"
+                            className="password-input"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <img
+                            src={showPassword ? EyeClosed : EyeOpen}
+                            alt={showPassword ? "Hide password" : "Show password"}
+                            className="password-eye-icon"
+                            onClick={toggleShowPassword}
+                        />
+                </div>
+
+                    <div className="error-message" style={{ visibility: error ? 'visible' : 'hidden', opacity: error ? 1 : 0 }}>
+                        이메일 또는 비밀번호가 올바르지 않습니다.
+                    </div>
+
+                <button className={"login-button"} onClick = {handleLogin}>
+                    <span className="login-text">LOGIN</span>
+                    <img src={ArrowIcon} alt="arrow" className="arrow-default" />
+                    <img src={ArrowIconClick} alt="arrow" className="arrow-active" />
+                </button>
+                </div>
+    </div>
     )   
 };
 
